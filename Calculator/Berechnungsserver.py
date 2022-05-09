55ef762b990b26552015e6a6b63cd2a3242126ff
Thaireis <tim.bieler@bluewin.ch>
2022-05-09 13:31:08 +0200

    Neustart Projekt
    Client / Server
    (Nur 2 Skripts)

--- Calculator/Berechnungsserver.py
+++ Calculator/Berechnungsserver.py
@@ -2,18 +2,49 @@ import sys
 from xmlrpc.server import SimpleXMLRPCServer
 
 argumentList = sys.argv
-hostAddress = '10.80.4.233'
+hostAddress = '10.80.4.215'
 port = '12345'
 server = SimpleXMLRPCServer((hostAddress, int(port)))
+
 
 def addition(x, y):
     return x + y
 
+
+def calculator2(x, y):
+    return x + y, x * y
+
+
+def calculator3(x, y):
+    return x - y, x / y
+
+
+def calculator4(x, y):
+    return x - y, x * y
+
+
+def calculator5(x, y):
+    return x + y, x / y
+
+
+def test(x, y, d, c):
+
+    username = "Timi"
+    password = 12345
+
+    if x == username and y == password:
+        return "Successful login", addition(d, c)
+    else:
+        return "Wrong Login"
+
+
 def subtraction(x, y):
     return x - y
 
+
 def multiplication(x, y):
     return x * y
+
 
 def division(x, y):
     try:
@@ -22,7 +53,7 @@ def division(x, y):
         return "You cannot divide 0"
 
 
-
+server.register_function(test, "test")
 server.register_function(addition)
 server.register_function(subtraction)
 server.register_function(multiplication)
--- Calculator/client.py
+++ Calculator/client.py
@@ -1,18 +1,35 @@
 import xmlrpc.client
 import sys
 
-argumentsList = sys.argv
-hostAddress = '10.80.4.233'
-hostPort = '12345'
 
+argumentsList = sys.argv
+hostAddress = '10.80.4.215'
+hostPort = '12345'
 URI = "http://" + hostAddress + ":" + hostPort
 
-num1 = int(input("Zahl 1: "))
-num2 = int(input("Zahl 2: "))
+
+# Eingaben
+username = (input("Username: "))
+password = int(input("Password: "))
+
+
+# Verbindung zum Server
 proxy = xmlrpc.client.ServerProxy(URI)
 
+# Input Zahlen
+num1 = int(input("Zahl 1: "))
+num2 = int(input("Zahl 2: "))
+
+# Ausgabe Login
+print('{}'.format(proxy.test(username, password, num1, num2)))
+
+
+# Ausgabe Berechnungen
+"""
 print('{} + {} is {}'.format(num1, num2, proxy.addition(num1, num2)))
 print('{} - {} is {}'.format(num1, num2, proxy.subtraction(num1, num2)))
 print('{} * {} is {}'.format(num1, num2, proxy.multiplication(num1, num2)))
 print('{} / {} is {}'.format(num1, num2, proxy.division(num1, num2)))
+"""
+
 print(URI)
